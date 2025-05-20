6b77003d814154c365696f3968f5ef67315d16e6
anjana732 <anjanasingh1257@gmail.com>
2025-05-20 15:52:07 +0530

    Connect MongoDb

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
@@ -7,6 +8,15 @@ const app = express();
 app.use(express.json);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`);
+
+connectDB()
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on ${PORT}`);
+    })
 })
+.catch(()=>{
+    console.log("DB connection failed");
+})
+
+
