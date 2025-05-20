40c527806d9509eba601846f9701e997e2a16afc
anjana732 <anjanasingh1257@gmail.com>
2025-05-20 23:38:53 +0530

    Add review route

--- routes/review.route.js
+++ routes/review.route.js
@@ -1,2 +1,12 @@
+import express from 'express';
+import { createReview, updateReview, deleteReview } from '../controllers/review.controller';
+import { verifyToken } from '../middlewares/auth.middleware';
 
+const router = express.Router();
+
+router.post('/:id/reviews', verifyToken, createReview);
+router.put('/:id',verifyToken, updateReview);
+router.delete('/:id', verifyToken, deleteReview);
+
+export default router;
 
